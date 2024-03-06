da953bef59c28593f27c54f7ada931e91a55a23e
Julien Sicot <112611461+superneyluj@users.noreply.github.com>
2024-03-06 15:20:51 +0100

    modification du code host de la multiplications de matrices

--- code/comparaison CPU FPGA/matrix_multiplication/alveo/host.cpp
+++ code/comparaison CPU FPGA/matrix_multiplication/alveo/host.cpp
@@ -1,9 +1,21 @@
+#define OCL_CHECK(error, call)                                                                   \
+        call;                                                                                        \
+        if (error != CL_SUCCESS) {                                                                   \
+            printf("%s:%d Error calling " #call ", error code is: %d\n", __FILE__, __LINE__, error); \
+            exit(EXIT_FAILURE);                                                                      \
+        }
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <CL/cl.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "xcl2/xcl2.hpp"
 
 #define MATRIX_SIZE 512
 #define DATA_SIZE (MATRIX_SIZE * MATRIX_SIZE)
+#define ITERATIONS 10
 
 int main(int argc, char** argv) {
 
@@ -13,10 +25,9 @@ int main(int argc, char** argv) {
     }
 
     std::string binaryFile = argv[1];
-    size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
     cl_int err;
     cl::Context context;
-    cl::Kernel krnl_vector_add;
+    cl::Kernel matrix_multiplication;
     cl::CommandQueue q;
 
     // Declarer et initialiser une matrice alignee avec des valeurs aleatoires
@@ -30,21 +41,53 @@ int main(int argc, char** argv) {
         std::generate(randomMatrixin2[i].begin(), randomMatrixin2[i].end(), std::rand);
     }
 
-    // Afficher la matrice 1
-    for (int i = 0; i < MATRIX_SIZE; ++i) {
-        for (int j = 0; j < MATRIX_SIZE; ++j) {
-            std::cout << randomMatrixin1[i][j] << " ";
+    auto devices = xcl::get_xil_devices();
+    auto fileBuf = xcl::read_binary_file(binaryFile);
+    cl::Program::Binaries bins{{fileBuf.data(),fileBuf.size()}};
+    bool valid_device = false;
+    for (unsigned int i = 0; i < devices.size(); i++) {
+        auto device = devices[i];
+        // Creating Context and Command Queue for selected Device
+        OCL_CHECK(err, context = cl::Context(device, nullptr, nullptr, nullptr, &err));
+        OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+        std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+        cl::Program program(context, {device}, bins, nullptr, &err);
+        if (err != CL_SUCCESS) {
+            std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
+        } else {
+            std::cout << "Device[" << i << "]: program successful!\n";
+            OCL_CHECK(err, matrix_multiplication = cl::Kernel(program, "matrix_multiplication", &err));
+            valid_device = true;
+            break; // we break because we found a valid device
         }
-        std::cout << std::endl;
+    }
+    if (!valid_device) {
+        std::cout << "Failed to program any device found, exit!\n";
+        exit(EXIT_FAILURE);
     }
 
-    // Afficher la matrice 2
-    for (int i = 0; i < MATRIX_SIZE; ++i) {
-        for (int j = 0; j < MATRIX_SIZE; ++j) {
-            std::cout << randomMatrixin2[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+
+    //Creating buffers
+    OCL_CHECK(err, cl::Buffer buffer_in1 = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * DATA_SIZE,randomMatrixin1.data()[0].data(), &err));
+    OCL_CHECK(err, cl::Buffer buffer_in2 = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * DATA_SIZE,randomMatrixin2.data()[0].data(), &err));
+    OCL_CHECK(err, cl::Buffer buffer_out = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * DATA_SIZE,Matrixresult.data()[0].data(), &err));
+
+    OCL_CHECK(err, err = matrix_multiplication.setArg(0, buffer_in1));
+    OCL_CHECK(err, err = matrix_multiplication.setArg(1, buffer_in2));
+    OCL_CHECK(err, err = matrix_multiplication.setArg(2, buffer_out));
+    OCL_CHECK(err, err = matrix_multiplication.setArg(3, MATRIX_SIZE));
+    OCL_CHECK(err, err = matrix_multiplication.setArg(4, ITERATIONS));
+
+    // Copy input data to device global memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_in1, buffer_in2}, 0 /* 0 means from host*/));
+
+    // Launch the Kernel
+
+    OCL_CHECK(err, err = q.enqueueTask(matrix_multiplication));
+
+    // Copy Result from Device Global Memory to Host Local Memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_out}, CL_MIGRATE_MEM_OBJECT_HOST));
+    q.finish();
 
 
     return 0;
